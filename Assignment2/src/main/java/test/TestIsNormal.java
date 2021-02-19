4696bf711fb784d678a41e71be06c4f2f796b53c
Maneesh <maneesh12325@gmail.com>
2021-02-19 17:00:17 -0500

    Assignment2 multiple user inputs

--- Assignment2/src/main/java/test/TestIsNormal.java
+++ Assignment2/src/main/java/test/TestIsNormal.java
@@ -14,53 +14,60 @@ import models.VitalSigns;
  * @author maneesh
  */
 public class TestIsNormal {
+
     public static void main(String[] args) {
-        
-                
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter patient name");
-        
-        Patient patient = new Patient();
-        patient.setName(scanner.nextLine());
-        
-        VitalSigns vitalSigns = new VitalSigns();
-        
-        System.out.println("Please enter current Respiratory rate");
-        vitalSigns.setResporatoryRate(scanner.nextInt());
-        
-        System.out.println("Please enter current Heart rate");
-        vitalSigns.setHeartRate(scanner.nextInt());
-        
-        System.out.println("Please enter current Systolic Blood Pressure");
-        vitalSigns.setBloodPressure(scanner.nextInt());
-        
-        System.out.println("Please enter current weight in KG");
-        vitalSigns.setWeight(scanner.nextDouble());
-        
-        System.out.println("If you want to enter age in months please enter M else enter Y");
-        String monthOrYear = scanner.next();
-        
+        boolean flag = true;
+        while (flag) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter patient name");
 
-        if (monthOrYear.equalsIgnoreCase("Y")){
-            System.out.println("Enter the age");
+            Patient patient = new Patient();
+            patient.setName(scanner.nextLine());
 
-            vitalSigns.setAge(scanner.nextDouble());
+            VitalSigns vitalSigns = new VitalSigns();
+
+            System.out.println("Please enter current Respiratory rate");
+            vitalSigns.setResporatoryRate(scanner.nextInt());
+
+            System.out.println("Please enter current Heart rate");
+            vitalSigns.setHeartRate(scanner.nextInt());
+
+            System.out.println("Please enter current Systolic Blood Pressure");
+            vitalSigns.setBloodPressure(scanner.nextInt());
+
+            System.out.println("Please enter current weight in KG");
+            vitalSigns.setWeight(scanner.nextDouble());
+
+            System.out.println("If you want to enter age in months please enter M else enter Y");
+            String monthOrYear = scanner.next();
+
+            if (monthOrYear.equalsIgnoreCase("Y")) {
+                System.out.println("Enter the age");
+
+                vitalSigns.setAge(scanner.nextDouble());
+            } else {
+                System.out.println("Enter the age in months");
+                double age = scanner.nextDouble() / 12;
+                vitalSigns.setAge(age);
+            }
+
+            boolean result = patient.isPatientNormal(vitalSigns);
+            if (result == true) {
+                System.out.println("Patient is normal");
+            } else {
+                System.out.println("Patient is not normal");
+            }
+            
+            System.out.println("Please enter Y if you want to continue");
+            String isContinue = scanner.next();
+            
+            if (!isContinue.equalsIgnoreCase("y")){
+                flag = false;
+            }
+
+            
+
         }
-        else{
-            System.out.println("Enter the age in months");
-            double age = scanner.nextDouble()/12;
-            vitalSigns.setAge(age);
-        }
-        
-        
-        boolean result = patient.isPatientNormal(vitalSigns);
-        if (result == true){
-            System.out.println("Patient is normal");
-        }
-        else{
-            System.out.println("Patient is not normal");
-        }
-        
-        
+
     }
 }
